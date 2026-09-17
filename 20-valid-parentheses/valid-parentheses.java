fd938a3a51b09522059155e35f899c2ccf34ad6b
vedika1011 <vedikat1011@gmail.com>
2026-09-17 13:46:39 +0530

    Time: 3 ms (85.99%) | Memory: 43.5 MB (25.59%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2!=0){
+            return false;
+        }
+        Stack<Character> stack=new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }
+            else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(c==')' && stack.peek()!='('){
+                    return false;
+                }
+                if(c==']' && stack.peek()!='['){
+                    return false;
+                }
+                if(c=='}' && stack.peek()!='{'){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+            return stack.isEmpty() ;
+        
+        
+
+    }
+}
