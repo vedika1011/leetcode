9402837b09853a6703d10d8ed613f92c20b31f26
vedika1011 <vedikat1011@gmail.com>
2026-09-21 19:17:26 +0530

    Time: 21 ms (5.48%) | Memory: 47.2 MB (5.50%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -8,16 +8,22 @@ class Solution {
             } catch(NumberFormatException e){
                 int b=stack.pop();
                 int a=stack.pop();
-                if(element.equals("+")){
-                    stack.push(a+b);
-                }
-                else if(element.equals("-")){
-                    stack.push(a-b);
-                }
-                else if(element.equals("*")){
-                    stack.push(a*b);
-                }else{
-                    stack.push(a/b);
+                // if(element.equals("+")){
+                //     stack.push(a+b);
+                // }
+                // else if(element.equals("-")){
+                //     stack.push(a-b);
+                // }
+                // else if(element.equals("*")){
+                //     stack.push(a*b);
+                // }else{
+                //     stack.push(a/b);
+                // }
+                switch(element){
+                    case "+":stack.push(a+b); break;
+                    case "-":stack.push(a-b); break;
+                    case "*":stack.push(a*b);break;
+                    case "/":stack.push(a/b);break;
                 }
             }
         }
