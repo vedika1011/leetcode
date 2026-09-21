2b2093559c0b5f2f8085ec5d9686dfb6e6d1b401
vedika1011 <vedikat1011@gmail.com>
2026-09-21 19:28:03 +0530

    Time: 6 ms (87.77%) | Memory: 45.1 MB (78.85%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,30 +1,44 @@
 class Solution {
     public int evalRPN(String[] tokens) {
+        // 8ms
+        // Stack<Integer> stack=new Stack<>();
+        // for(String element : tokens){
+        //     try{
+        //         int n=Integer.parseInt(element);
+        //         stack.push(n);
+        //     } catch(NumberFormatException e){
+        //         int b=stack.pop();
+        //         int a=stack.pop();
+        //         switch(element){
+        //             case "+":stack.push(a+b); break;
+        //             case "-":stack.push(a-b); break;
+        //             case "*":stack.push(a*b);break;
+        //             case "/":stack.push(a/b);break;
+        //         }
+        //     }
+        // }
+        // return stack.pop();
+
         Stack<Integer> stack=new Stack<>();
-        for(String element : tokens){
-            try{
-                int n=Integer.parseInt(element);
-                stack.push(n);
-            } catch(NumberFormatException e){
+        for(String c:tokens){
+            if(c.equals("+")){
+                stack.push(stack.pop()+stack.pop());
+            }
+            else if(c.equals("-")){
                 int b=stack.pop();
-                int a=stack.pop();
-                // if(element.equals("+")){
-                //     stack.push(a+b);
-                // }
-                // else if(element.equals("-")){
-                //     stack.push(a-b);
-                // }
-                // else if(element.equals("*")){
-                //     stack.push(a*b);
-                // }else{
-                //     stack.push(a/b);
-                // }
-                switch(element){
-                    case "+":stack.push(a+b); break;
-                    case "-":stack.push(a-b); break;
-                    case "*":stack.push(a*b);break;
-                    case "/":stack.push(a/b);break;
-                }
+                int a =stack.pop();
+                stack.push(a-b);
+            }
+            else if(c.equals("*")){
+                stack.push(stack.pop()*stack.pop());
+            }
+            else if(c.equals("/")){
+                int den=stack.pop();
+                int num=stack.pop();
+                stack.push(num/den);
+            }
+            else{
+                stack.push(Integer.parseInt(c));
             }
         }
         return stack.pop();
